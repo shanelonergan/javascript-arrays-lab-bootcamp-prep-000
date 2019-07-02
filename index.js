e0873d4a027e9990df897b70bad392cf162af342
Shane Lonergan <sptlonergan@gmail.com>
2019-07-02 15:11:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,25 @@ function destructivelyAppendKitten(name) {
 }
 
 destructivelyAppendKitten('Eloise');
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+destructivelyPrependKitten('Eloise');
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+destructivelyRemoveLastKitten()
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+
+destructivelyRemoveFirstKitten()
+  
