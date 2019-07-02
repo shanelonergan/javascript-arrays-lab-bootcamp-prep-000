6d3588be574220e38b835e78a277cf9f8c28d661
Shane Lonergan <sptlonergan@gmail.com>
2019-07-02 14:50:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,31 +1,3 @@
 var kittens = ['Milo', 'Otis', 'Garfield'];
 
-function destructivelyAppendKitten(array, name) {
-  array.push(name);
-  return array;
-}
 
-destructivelyAppendKitten(kittens, 'Eloise');
-
-function destructivelyPrependKitten(array, name) {
-  aray.unshift(name);
-  return kittens
-}
-
-destructivelyPrependKitten(kittens, Eloise)
-
-function destructivelyRemoveLastKitten(kittens) {
-  kittens.pop();
-  return kittens;
-}
-
-destructivelyRemoveLastKitten(kittens)
-
-function destructivelyRemoveFirstKitten(kittens) {
-  kittens.shift();
-  return kittens;
-}
-
-destructivelyRemoveFirstKitten(kittens)
-
-kittens = ['Milo', 'Otis', 'Garfield'];
