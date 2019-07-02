9d5f876fb6112f0bc25dc0f6675c91ad8ac3d1ea
Shane Lonergan <sptlonergan@gmail.com>
2019-07-02 14:51:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(array, name) {
+  array.push(name);
+  return array;
+}
+
+destructivelyAppendKitten(kittens, 'Eloise');
